dd7af1e04040d1ae00a6d0f999699761b7a4fc2a
CarlosZeyy <carloszeeyy@gmail.com>
2026-05-16 00:55:13 -0300

    feat: enhance error handling in API response interceptor with token refresh logic

--- src/service/api.ts
+++ src/service/api.ts
@@ -25,11 +25,33 @@ api.interceptors.response.use(
   (response) => {
     return response;
   },
-  (error) => {
-    if (error.response && (error.response.status === 401 || error.response.status === 403)) {
-      window.location.href = "/refresh";
+  async (error) => {
+    const originalRequest = error.config;
 
-      return Promise.reject(error);
+    if (
+      (error.response?.status === 401 ||
+        error.response?.status === 403 ||
+        error.response?.status === 500) &&
+      !originalRequest._retry
+    ) {
+      originalRequest._retry = true;
+
+      try {
+        const { data } = await axios.post(`${BASE_URL}/auth/refresh`, {
+          refreshToken: localStorage.getItem("refreshToken"),
+        });
+
+        originalRequest.headers.Authorization = `Bearer ${data.token}`;
+        localStorage.setItem("token", data.token);
+        localStorage.setItem("refreshToken", data.refreshToken);
+
+        return api(originalRequest);
+      } catch (error) {
+        localStorage.removeItem("token");
+        localStorage.removeItem("refreshToken");
+        window.location.href = "/";
+        return Promise.reject(error);
+      }
     }
 
     return Promise.reject(error);
