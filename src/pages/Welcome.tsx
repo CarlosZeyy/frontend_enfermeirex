43b6a3b32fbba02bdc9b9173085fdb494d9d62ad
CarlosZeyy <carloszeeyy@gmail.com>
2026-05-17 12:07:57 -0300

    feat: enhance Welcome page layout with improved styling and structure

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,22 +1,28 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const Welcome = () => {
   return (
     <div>
-        <h1>EnfermeirEX</h1>
-        <p>Sua saúde, no conforto de casa.</p>
+      <h1 className="text-center font-semibold">
+        Enferm<span className="text-red-600">EX</span>
+      </h1>
+      <p className="text-center font-medium pt-3">
+        Sua saúde, no conforto de casa.
+      </p>
 
-        <p>Ja possui conta em nosso sistema? faça o login</p>
-        <Link to={"/login"}>Login</Link>
+      <p className="text-center">
+        Ja possui conta em nosso sistema? faça o login
+      </p>
+      <Link to={"/login"} className="">
+        Login
+      </Link>
 
-        <Link to={"/forgot-password"}>Esqueceu a senha?</Link>
-        
-        <p>Caso não tenha se cadastrado ainda faça o seu registro aqui</p>
-        <Link to={"/register"}>Registrar</Link>
+      <Link to={"/forgot-password"}>Esqueceu a senha?</Link>
 
-
+      <p>Caso não tenha se cadastrado ainda faça o seu registro aqui</p>
+      <Link to={"/register"}>Registrar</Link>
     </div>
-  )
-}
+  );
+};
 
-export default Welcome
+export default Welcome;
